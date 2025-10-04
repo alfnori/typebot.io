ccaaf863c4f670205994e91d8ca08d8b53a3a9da
Adolfo Luiz <alfnori@gmail.com>
2025-10-04 12:50:17 -0300

    chore: fixes build vitest configuration

--- apps/builder/vitest.config.ts
+++ apps/builder/vitest.config.ts
@@ -1,9 +1,13 @@
+import type { PluginOption } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+// vitest.config.ts
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [tsconfigPaths() as PluginOption], // cast to satisfy TypeScript
   test: {
     include: ["**/*.test.ts"],
+    globals: true, // optional, depends on your setup
+    environment: "jsdom", // optional, or "node"
   },
 });
