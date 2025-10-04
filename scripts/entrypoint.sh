8df94375e28818ed58ccbf6213f9190336350539
Adolfo Luiz <alfnori@gmail.com>
2025-10-04 14:55:40 -0300

    chore: fixing entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,26 +3,13 @@ set -e
 
 echo "Starting entrypoint for scope: ${SCOPE}"
 
-# If scope is builder, run Prisma migrate
-if [ "$SCOPE" = "builder" ]; then
-  echo "Configuring runtime environment for Next.js..."
-  cd apps/builder
-  node -e "const { configureRuntimeEnv } = require('next-runtime-env/build/configure'); configureRuntimeEnv();"
-  cd ../..
-
-  export DATABASE_URL=${DATABASE_URL}
-
-  echo "Running Prisma migrate for builder..."
-  ./node_modules/.bin/prisma migrate deploy --schema=packages/prisma/schema.prisma
-fi
-
 # Determine which app to start
 case "$SCOPE" in
   builder)
-    APP_PATH="apps/builder/server.js"
+    APP_PATH="apps/builder"
     ;;
   viewer)
-    APP_PATH="apps/viewer/server.js"
+    APP_PATH="apps/viewer"
     ;;
   *)
     echo "Unknown SCOPE: $SCOPE"
@@ -30,5 +17,18 @@ case "$SCOPE" in
     ;;
 esac
 
-echo "Starting Node app at $APP_PATH..."
-NODE_OPTIONS=--no-node-snapshot HOSTNAME=${HOSTNAME:-0.0.0.0} PORT=${PORT:-3000} node "$APP_PATH"
+echo "Configuring runtime environment for Next.js..."
+cd $APP_PATH
+node -e "const { configureRuntimeEnv } = require('next-runtime-env/build/configure'); configureRuntimeEnv();"
+cd ../..
+
+# If scope is builder, run Prisma migrate
+if [ "$SCOPE" = "builder" ]; then
+  export DATABASE_URL=${DATABASE_URL}
+
+  echo "Running Prisma migrate for builder..."
+  ./node_modules/.bin/prisma migrate deploy --schema=packages/prisma/schema.prisma
+fi
+
+echo "Starting Node app at $APP_PATH/server.js..."
+NODE_OPTIONS=--no-node-snapshot HOSTNAME=${HOSTNAME:-0.0.0.0} PORT=${PORT:-3000} node "$APP_PATH/server.js"
